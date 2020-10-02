9466ec868b40a8cffd855d5297d41abd543becac
Viet Tran <vietnamtran@gmail.com>
2020-10-02 19:44:03 +0000

    Added setters getters type and value check

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,8 +7,51 @@ class rectangle():
 
     
     def __init__(self, *args, **kwargs):
+        """Init for square """
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+    @property
+    def width(self):
+        """ getter method for width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        setter method with type and value check
+
+            Parameter:
+                value: width of square
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """ getter method for height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        setter method with type and value check
+
+            Parameter:
+                value: height of square
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
 
     def area_of_my_rectangle(self):
         """ Area of the square """
